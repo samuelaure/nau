fca22cf69e56d3f4012c0c3011b1878f88f659c4
Samuel Aure <samuelaure@gmail.com>
2025-08-18 14:32:52 +0200

    style: setup and apply more extensive formatting

--- packages/nau-core/jest.config.js
+++ packages/nau-core/jest.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  preset: 'ts-jest', 
+  preset: 'ts-jest',
   rootDir: '.',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   testRegex: 'src/.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': [
-      'ts-jest', 
-      { 
-        tsconfig: path.resolve(__dirname, 'tsconfig.test.json')
-      }
+      'ts-jest',
+      {
+        tsconfig: path.resolve(__dirname, 'tsconfig.test.json'),
+      },
     ],
   },
   testEnvironment: 'node',
-};
+}
