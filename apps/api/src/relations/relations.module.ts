a074c42f38881fb8524b73e07e30d98bda995915
Samuel Aure <samuelaure@gmail.com>
2026-04-18 22:40:02 +0200

    fix(api): provide PrismaModule to RelationsModule

--- apps/api/src/relations/relations.module.ts
+++ apps/api/src/relations/relations.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RelationsController } from './relations.controller';
 import { RelationsService } from './relations.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [RelationsController],
   providers: [RelationsService],
 })
