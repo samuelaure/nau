f2edd1c7e1d53c5d6e45f732368172710aeb2e6d
Samuel Aure <samuelaure@gmail.com>
2026-05-03 17:08:30 +0200

    fix(api): remove unsupported envPrefix from createStorage call

--- apps/api/src/media/storage.service.ts
+++ apps/api/src/media/storage.service.ts
@@ -13,10 +13,8 @@ export class StorageService implements OnModuleInit {
     const secretAccessKey = this.configService.get<string>('R2_SECRET_ACCESS_KEY', '');
     const bucket = this.configService.get<string>('R2_BUCKET_NAME', '');
     const publicUrl = this.configService.get<string>('R2_PUBLIC_URL', '');
-    const envPrefix = this.configService.get<string>('NODE_ENV', 'development');
-
     if (endpoint && accessKeyId && secretAccessKey && bucket && publicUrl) {
-      this.storage = createStorage({ endpoint, accessKeyId, secretAccessKey, bucket, publicUrl, envPrefix });
+      this.storage = createStorage({ endpoint, accessKeyId, secretAccessKey, bucket, publicUrl });
     }
   }
 
